949530426d6887168c526cf8f227f3e8ea28d8d0
Djieuga Djoubi <djieugadjoubirylenathan@gmail.com>
2023-04-14 18:27:32 +0100

    Added a new template:singlepage/index.html and urls to serve this template

--- edxcs50_djangoapp/myapp/urls.py
+++ edxcs50_djangoapp/myapp/urls.py
@@ -3,5 +3,7 @@ from . import views
 app_name = 'myapp'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:name>', views.greet, name='greet'),
+    # path('<str:name>', views.greet, name='greet'),
+    path('singlepage', views.singlepage, name='singlepage'),
+    path('singlepage/sections/<int:num>', views.section, name='section'),
 ]
--- edxcs50_djangoapp/myapp/views.py
+++ edxcs50_djangoapp/myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 
 # Create your views here.
 
@@ -9,3 +9,18 @@ def greet(request, name):
     return render(request, 'home/greet.html', {
         'name': name.capitalize()
     })
+
+def singlepage(request):
+    return render(request, 'singlepage/index.html')
+
+texts = [ 
+    'Text 1 lorem ipsomjsddjsdsjdhjdjsdjsdskjdsjdjsdjsdsjdskjd ssdjslkdjsaodids sdkshdkjshdsajdsad skdusdsahd sdksadhaksdsd sdsuhdsdjoiaddjpoisdpsa sidhsad', 
+    'Text 2 sth ewiajd uasodosdodsdnsjdssdh sdaabaawiwdajwowpapjad didioeiurb ju nythsaodalaufewij uwaiuiefnaaioL uufeyuejhifkweorijn uiueiuffe iuifeuowq;foijaoffhufn hguaygif', 
+    'Text 3 uo ooduhf trhetjj dudospweijffueu duwiiwkfdjnnookdihf yrieodnhussaodfjfnfuffnf idjdodihnkkdndhdhdijusjsodsj usjsoseydhjkwlewfjdkfkdfkjsijslkjdldourdifjdkudfdfjdnf',
+    ]
+def section(request, num):
+    if 1 <= num <= 3:
+       return HttpResponse(texts[num - 1])
+    else:
+        raise Http404('No such section')
+    
