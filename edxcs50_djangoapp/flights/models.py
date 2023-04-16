39750c2f0ae3d97aa7e2822a61703f047fbeaa6d
Djieuga Djoubi <djieugadjoubirylenathan@gmail.com>
2023-04-16 19:00:28 +0100

    server-side testing

--- edxcs50_djangoapp/flights/models.py
+++ edxcs50_djangoapp/flights/models.py
@@ -15,6 +15,9 @@ class Flight(models.Model):
 
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
+    def is_valid_flight(self):
+        return self.origin != self.destination or self.duration > 0
+    
 
 class Passenger(models.Model):
     first = models.CharField(max_length=64)
